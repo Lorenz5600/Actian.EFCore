7bb97f3f195508b1f035678b64368d91521420c4
Morten Houston Ludvigsen <mlu@2ps.dk>
2023-02-08 17:33:21 +0100

    Fix some of UpdatesActianTest

--- test/Actian.EFCore.FunctionalTests/UpdatesActianTest.cs
+++ test/Actian.EFCore.FunctionalTests/UpdatesActianTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Actian.EFCore.TestUtilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.TestModels.UpdatesModel;
@@ -7,7 +8,7 @@ using Xunit.Abstractions;
 
 namespace Actian.EFCore
 {
-    public class UpdatesActianTest : UpdatesRelationalTestBase<UpdatesActianFixture>
+    public class UpdatesActianTest : UpdatesRelationalTestBase<UpdatesActianFixture>, IDisposable
     {
         public UpdatesActianTest(UpdatesActianFixture fixture, ITestOutputHelper testOutputHelper)
             : base(fixture)
@@ -18,147 +19,71 @@ namespace Actian.EFCore
 
         public ActianSqlFixtureHelpers Helpers { get; }
         public void AssertSql(params string[] expected) => Helpers.AssertSql(expected);
-        public void LogSql() => Helpers.LogSql();
+
+        public void Dispose() => Helpers.LogSql();
 
         public override void Mutation_of_tracked_values_does_not_mutate_values_in_store()
         {
-            try
-            {
-                base.Mutation_of_tracked_values_does_not_mutate_values_in_store();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Mutation_of_tracked_values_does_not_mutate_values_in_store();
         }
 
         public override void Save_partial_update()
         {
-            try
-            {
-                base.Save_partial_update();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Save_partial_update();
         }
 
         [ActianTodo]
         public override void Save_partial_update_on_missing_record_throws()
         {
-            try
-            {
-                base.Save_partial_update_on_missing_record_throws();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Save_partial_update_on_missing_record_throws();
         }
 
         [ActianTodo]
         public override void Save_partial_update_on_concurrency_token_original_value_mismatch_throws()
         {
-            try
-            {
-                base.Save_partial_update_on_concurrency_token_original_value_mismatch_throws();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Save_partial_update_on_concurrency_token_original_value_mismatch_throws();
         }
 
         [ActianTodo]
         public override void Update_on_bytes_concurrency_token_original_value_mismatch_throws()
         {
-            try
-            {
-                base.Update_on_bytes_concurrency_token_original_value_mismatch_throws();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Update_on_bytes_concurrency_token_original_value_mismatch_throws();
         }
 
         public override void Update_on_bytes_concurrency_token_original_value_matches_does_not_throw()
         {
-            try
-            {
-                base.Update_on_bytes_concurrency_token_original_value_matches_does_not_throw();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Update_on_bytes_concurrency_token_original_value_matches_does_not_throw();
         }
 
         [ActianTodo]
         public override void Remove_on_bytes_concurrency_token_original_value_mismatch_throws()
         {
-            try
-            {
-                base.Remove_on_bytes_concurrency_token_original_value_mismatch_throws();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Remove_on_bytes_concurrency_token_original_value_mismatch_throws();
         }
 
         public override void Remove_on_bytes_concurrency_token_original_value_matches_does_not_throw()
         {
-            try
-            {
-                base.Remove_on_bytes_concurrency_token_original_value_matches_does_not_throw();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Remove_on_bytes_concurrency_token_original_value_matches_does_not_throw();
         }
 
         public override void Can_remove_partial()
         {
-            try
-            {
-                base.Can_remove_partial();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Can_remove_partial();
         }
 
         [ActianTodo]
         public override void Remove_partial_on_missing_record_throws()
         {
-            try
-            {
-                base.Remove_partial_on_missing_record_throws();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Remove_partial_on_missing_record_throws();
         }
 
         [ActianTodo]
         public override void Remove_partial_on_concurrency_token_original_value_mismatch_throws()
         {
-            try
-            {
-                base.Remove_partial_on_concurrency_token_original_value_mismatch_throws();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.Remove_partial_on_concurrency_token_original_value_mismatch_throws();
         }
 
-        [ActianTodo]
+
         public override void Save_replaced_principal()
         {
             base.Save_replaced_principal();
@@ -176,12 +101,10 @@ namespace Actian.EFCore
                 ",
                 @"
                     @p1='78'
-                    @p0='New Category' (Size = 4000)
+                    @p0='New Category'
                     
-                    SET NOCOUNT ON;
                     UPDATE ""Categories"" SET ""Name"" = @p0
-                    WHERE ""Id"" = @p1;
-                    SELECT @@ROWCOUNT;
+                    WHERE ""Id"" = @p1
                 ",
                 @"
                     SELECT FIRST 2 ""c"".""Id"", ""c"".""Name"", ""c"".""PrincipalId""
@@ -199,26 +122,12 @@ namespace Actian.EFCore
 
         public override void SaveChanges_processes_all_tracked_entities()
         {
-            try
-            {
-                base.SaveChanges_processes_all_tracked_entities();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.SaveChanges_processes_all_tracked_entities();
         }
 
         public override void SaveChanges_false_processes_all_tracked_entities_without_calling_AcceptAllChanges()
         {
-            try
-            {
-                base.SaveChanges_false_processes_all_tracked_entities_without_calling_AcceptAllChanges();
-            }
-            finally
-            {
-                LogSql();
-            }
+            base.SaveChanges_false_processes_all_tracked_entities_without_calling_AcceptAllChanges();
         }
 
         public override void Identifiers_are_generated_correctly()
